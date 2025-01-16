b3a7308f6c714aa3c5705859caad7abb0b257207
tmcmm <tiagomendes93@hotmail.com>
2025-01-16 12:31:19 +0000

    Update variables.tf
    my public_ip var required

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -111,3 +111,9 @@ variable "worker_vm_count" {
   type    = number
   default = 2
 }
+
+# curl -4 ifconfig.io
+variable "mypublic_ip" {
+  description = "My provider public IP"
+  type        = string
+}
