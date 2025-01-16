98eb67959d99c14520158ee562fd97289327f593
tmcmm <tiagomendes93@hotmail.com>
2025-01-16 14:38:18 +0000

    Update variables.tf

--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -50,12 +50,12 @@ variable "tags" {
   default     = {}
 }
 
-# curl -4 ifconfig.io
-#variable "mypublic_ip" {
-#  description = "My provider public IP"
-#  type        = string
+#curl -4 ifconfig.io
+variable "mypublic_ip" {
+  description = "My provider public IP"
+  type        = string
 #  default     = ""
-#}
+}
 
 variable "os_disk_image" {
   type        = map(string)
